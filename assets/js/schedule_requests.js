dc49aea42f7e271cfec79e5b6eeff731c8d7aa84
Garrett Hollar <hollargarrett@gmail.com>
2019-12-07 17:24:31 -0500

    making requests

---
+++ assets/js/schedule_requests.js
@@ -0,0 +1,29 @@
+var scheduleRequest = new Vue({
+    el: '#schedule-requests',
+    data: {
+        maxCredits = "",
+        semestersOff: [],
+        semester = ''
+    },
+    beforeCreate: function() {
+        console.log(this.$cookies.isKey("jwt"));
+        axios.get('http://josh.danilafe.com:23450/myinfo')
+            .then(response => {
+                console.log(response)
+                if (response.data.ID == 0){
+                    console.log("Not logged in, returning to index");
+                    window.location.href = "/index.html";
+                }
+            })
+            .catch(function(error){
+                console.log("didn't work");
+                console.log(error);
+            });
+    },
+
+    methods: {
+        addSemester () {
+            this.semestersOff.push(semester);
+        }
+    }
+})
