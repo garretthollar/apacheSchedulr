82adafba4a7c2387ec2c38c148fb05b898a39e99
Garrett Hollar <hollargarrett@gmail.com>
2019-12-08 13:58:01 -0500

    Update classes_taken.js

--- assets/js/classes_taken.js
+++ assets/js/classes_taken.js
@@ -6,7 +6,7 @@ var classTable = new Vue({
     created: function() {
         axios.get('https://api.schedulr.xyz/my_needed',{
             headers: {
-                Authorization: this.$cookies.get("access_token_cookie")
+                Authorization: 12
             }
         })
             .then(response=> {
