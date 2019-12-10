cc63ef30db54b4b80e92224e791ee3ed0f177e18
Garrett Hollar <hollargarrett@gmail.com>
2019-12-09 19:14:59 -0500

    majors edits

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -9,16 +9,6 @@ var profile = new Vue({
         myMajors: []
     },
     mounted: function() {
-        axios.get('http://josh.danilafe.com:23450/myinfo')
-            .then(response=> {
-                console.log(response);
-                this.form.FirstName = response.data.FirstName;
-                this.form.LastName = response.data.LastName;
-                this.form.Email = response.data.Username;
-            })
-            .catch(error => {
-                console.log(error);
-            })
         axios.get('https://api.schedulr.xyz/list_programs')
             .then(response=> {
                 this.majors = response.data;
@@ -30,13 +20,31 @@ var profile = new Vue({
         
     },
     methods: {
-        getPrograms () {
+        getPrograms() {
             axios.get('https://api.schedulr.xyz/list_programs')
             .then(response=> {
                 this.myMajors = response.data;
             })
             .catch(error=> {
                 console.log(error);
+            })
+        },
+        addProgram () {
+            axios.post('https://api.schedulr.xyz/add_program',{
+                headers: {
+                    Authorization: "Bearer " + this.$cookies.get("access_token_cookie")
+                },
+                prog_id: 27
+            })
+            .then(response => {
+                if (response.status == 200)
+                {
+                    console.log("Program added!");
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                console.log(this.$cookies.get("access_token_cookie"));
             })
         }
     }
